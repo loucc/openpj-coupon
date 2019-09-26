4f94c2ef64096c387ea60eaeb2525a321a2d466f
loucongcong <lou.congcong@iwhalecloud.com>
2019-09-26 10:34:26 +0800

    '新增微服务通用模块'

---
+++ openpj-coupon-service/coupon-common/src/main/java/com/openpj/coupon/annotation/IgnoreResponseAdvice.java
@@ -0,0 +1,15 @@
+package com.openpj.coupon.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <h1>忽略统一响应注解定义</h1>
+ * Created by loucc.
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IgnoreResponseAdvice {
+}
---
+++ openpj-coupon-service/coupon-common/src/main/java/com/openpj/coupon/exception/CouponException.java
@@ -0,0 +1,12 @@
+package com.openpj.coupon.exception;
+
+/**
+ * <h1>优惠券项目通用异常定义</h1>
+ * Created by loucc.
+ */
+public class CouponException extends Exception {
+
+    public CouponException(String message) {
+        super(message);
+    }
+}
